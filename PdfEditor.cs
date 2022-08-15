09afe439855b1a12939f7afe9961ac092c169855
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2022-08-15 23:21:02 +0100

    Add files via upload

--- PdfEditor.cs
+++ PdfEditor.cs
@@ -12,12 +12,11 @@ namespace PdfFormEditor
         internal static PDFDoc ScannedDocument;
         internal string FilePath = "";
         internal static PDFPage Page;
-        internal static PDFNameTree NameTree;
-        internal PDFNameTree NewTemplateNameTree;
         private static int pageCount;
         private static int width;
         private static int height;
-        private string savedTemplate = "pageTemplate";
+        //The template name "Employee" is already embedded in the demo file, you can assign any name to your templates when using the SDK.
+        private string savedTemplate = "Employee";
 
         public PdfEditor()
         {
@@ -25,7 +24,7 @@ namespace PdfFormEditor
             ScannedDocument = LoadDocument();            
         }
 
-        
+
         private PDFDoc LoadDocument()
         {
             try
@@ -39,8 +38,8 @@ namespace PdfFormEditor
                 {
                     //For the purpose of this demo, the keys are used within the application.
                     //In a real world application, they should be treated as secret keys and protected accordingly
-                    string sn = "ot6HkKxmynOV6H7SCs0bZ1F8EwyMt8OY4Nc6hZZQaKpqedyGmwh2/w==";
-                    string key = "8f0YFUNsvWkJ+VdwlrBxubTyFADaUzKsCTnsn3Str88uZqvOxGiG5Ovnvl3bGrsELZ0tlOE2mMiHkC/wMeMhm50ruBfjT39N2dV7TdoAiwDSDrfMr2MseAuwHHleoOIxy141Ld4JOWA6ukSanwSfRk2XHhT8Fb+D9RDiRMeutt9EbtXs38KdIXNuvydGhgPa1FF2UUbzuFilXtuMIDMoLYA00p2ZuEhU83fIUxfW9LGsBQ9/tyeXGhcjG1dwvU81jUDkav4v2+osM5ZVpx4T2Q6Vdm4/AOXDWgOZY7iXbm4tZs8XaTIgoUfNC8a2DcDCUHrfiOGeo+4fjAWmYgtAq82CkfeKQlgxDubRn4MLd4hq2CVbUQciGCi/z5el7dPE5SokErJPmLGqzfqUWoMWgM0d3dvcRb5O54SanMy+0Z4oNMwrepRZWbDVZHsfi0FMybETagUBo0s0cDZRwWUGJgOGJXzTVh163lyGkuqNxVv4d7Z5WpbNvm8H8ep5d5YA6+GJ5LSSnUSXZIc1wGDQUAn7SHhSbimO+OG5uhXGedrdKXpoDAXl/12OJbbkHg+IbxIlUATSNKMfMqxQJu1F10CO+R+fHByfzbou+cfMPddlpkkIVJ0tMSV+D8UQ3f0W+xC3gYp8PPG7IzSw293AuRxKj2t2P9PdXkDYcaeRkrO4M8SjwmSQkpVAbULA/eFLSidKJysW+EzelHB9m6yoLu9C0spCJeRi0V8+RN4xChMmmlVf0SifwSBhuCO4mfUKd8H5SFz+0Y6nKPFQ0i2BlYOk24CN1uSXnXmz6Hy7wtBqPah8RATUN/QiUpBKodxykH0VceCLpZo+DfgmKEu1QF7rJTJcvQCWDUOQ64iDXEhqFBReimZKX7VRi2u5ZNLYrxqkr8calhjKwr5Fz6RBKt2lgYwuiEFOJpLc7ZJjn3X+5y+fN1k3r0H9oj+sJqlaVOLaNwhFywVqjIUoqGVexql0HUcmMtY4r2HOHoJrHsIaq8xkhf/aHa5Ievc3NhyvSphG5IFIiuWMMB1zqMbCaim1IQjdCJMUJN+jPDTLgFbar2E6nPUT0dWlWcxwt6J2+0IphRUpEQYbeiMB1USPrW0t5Odzayf5o47kZCPyqsCHh/FWYV0grLa8rfYgg1RIxsppxK6jd9DfHT6Pr70YzG9r9j03c3wisCDZusYmEoSSe1B2l+ICI/UNBVgTtsiEp9IxTBqMOIX8wHO7w8xMEwwIOR65ZMLGqXwjPrYKdrA3JBTEUw/07aUvarLFhFE77UvvLyp/amWmuORahjaX1RbPsqkNoCpDjDqC0edu30UnIYNLV2JqIg==";
+                    string sn = "SXC5wsuLUEUO4OurtXCk18JnGcxp6hrx5I1X8TbpPYqwzwQ3vWxOkg==";
+                    string key = "8f0YFUGMvRkN+ddwhrBxubRv+38Gz2ILMFDAylGsl9gEt83edTI+7gO6BL3Zwz4kg8iFbUBbTA0Fo1AkxlPp2d40Z8iMd8SHfgzBN4RXdnt8UAB+qfAI8OrWhPtzxO+yOivYHoRYeiFttZWqwI4X1fwdUGO37hYJVsD/EdiOZqAjvZBeI95cx5fY0CtsvH+NaMagwb6HwBNDT0dI45XNR9ycMEboU+Ia211DtwOplnZ25dOssAtBPNk21UxQnmZMQEwfr//15ssa7/OkxfZDJY1AlID9Vq8StY3iJCSn4q+t0dSoJkYM/lhZNkkn7OGPTjjx/WJ0yXN1SISFvrHgVbnLhwtVUnJ7wCz91NLIISUTmWRJRcEXu7CQmS4+QjDhBbtKAyG4urX9uNzkkn2Xbk/QN1D5Id6fYOrLxVwmjllIW7JASUgxpBRbt3qxYUuIZGxtu009yTB/NATboZS2FMngZD556sCMsT7Xb1JQwqyQ2WBRSm31qWi8g87e3MewMGaNW5T0P/NEqU619EX0RUiedSCm6ISqCeGXKqw1jJ4K56293J31qvn1DfwePzHg3O+Ga2eqHqERaNbCqtwGHpGo/DAa/r9qhipsO/W8BGfjDzRs9zvKvnjwIkoOoI/4HiQapyFG6F6rDW/C78gubmNjASjK9C9NtJ12F/qohlErie4gd8gik+L12bfrZPXs8j9pMNHFbzT50zYuRv0qt/f38goLxjKZkVS2V5do9n1ehlVf0Sq/EXzz+XX9xIjh6fL/0DykoTOXm8i/J40BOZ1e+0sDkI0vrYw6AvnFz1AKB6p4ROQyFUdmt5pRNlWRTMYxgrFf+jZ6g6LkmTd+d0+MT2MX45R2xKWF3JIiyrtZXxXplMur7dKe9B8B7rrJ8klvOnJzP8Buj4fHjxp8EFbBvscng1H5wdYrxB/PlWmH4ZY0iPn5zSP8Uauvy2osIRk/LadTjSSi3+z1XY7MkGai86eiu2EjQtlnnnEz0XWaguOJueVPErt7xGDgihqOeptG5nMwNi/S98/UX63lW/r9UNR40tN/1gGEropGpfHvDuviLWn8ANieWcxxlyL1U8D02lPoND/kfNBEsQY02ZWB+pZ2Qk3pLBuYL+YvkFabCyBXYVCp6Mjj7DYgg8jRPdtuxK5X9i4LveA9ViQI2tQcMfCsxS0YUf8bLxa4Yp7xJKfosFBACGSUU12XNaOEp9IrTkaXaqi+KTKUy5SI2JN5zD6Wjid43oDpi16eV39dLVlrRWI5MITj8rBrxg0CWyTLOwJ/um2vIqH1K9br27x0L5paRTxc9N7WIZci1gnxC8Jzjs5qwg==";
                     ErrorCode error_code = Library.Initialize(sn, key);
                     if (error_code != ErrorCode.e_ErrSuccess)
                     {
@@ -77,7 +76,7 @@ namespace PdfFormEditor
                     foxit.common.Image image = new foxit.common.Image();
                     string imgPath = $"{FilePath.Split('.').First()}IndexPage.jpg";
                     image.AddFrame(bitmap);
-                    image.SaveAs(imgPath);                    
+                    image.SaveAs(imgPath);
                     DocumentImage.Image = System.Drawing.Image.FromFile(imgPath);
                 }
                 return ScannedDocument;
@@ -94,49 +93,19 @@ namespace PdfFormEditor
             {
                 //Adding a counter helps to track how many times the "Add Page" button was clicked in order to handle the creation of multiple pages
                 pageCount++;
-                
-                if (pageCount < 2)
-                {
-                    // "Page" below is used to retrieve specific page, in this case, the first page.
-                    Page = ScannedDocument.GetPage(0);
-                   
-                    //"PDFNameTree" is used when converting creating a template out of a document. 
-                    NameTree = new PDFNameTree(ScannedDocument, PDFNameTree.Type.e_Pages);
 
-                    //The first input parameter "pageTemplate" is used to give the template a name.
-                    //The template name can be used to subsequently call and manipulate the PDF document template.
-                    NameTree.Add(savedTemplate, Page.GetDict());
-                    //It's important to hide the page before you can use "HidePageTemplate" to add the page to the document.
-                    ScannedDocument.HidePageTemplate(savedTemplate);
-                    //"AddPageFromTemplate" is used to add a hidden page template to the document.
-                    Page = ScannedDocument.AddPageFromTemplate(savedTemplate);
-                    //I hardcoded "TemplatePage" to the file path to save the new template document to a new file rather than updating the original file
-                    //As a personal convention, this can help in reconciling the original document with the template document
-                    ScannedDocument.SaveAs($"{FilePath.Split('.').First()}TemplatePage.pdf", (int)PDFDoc.SaveFlags.e_SaveFlagNoOriginal);
-                    int pageNumbers = ScannedDocument.GetPageCount();
-                }
-                else
-                {
-                    //The "pageIndexPosition" is used to get the location of the first template page (which changes as new pages are added).
-                    int pageIndexPosition = pageCount - 2;                    
-                    Page = ScannedDocument.GetPage(pageIndexPosition);
-                    NewTemplateNameTree = new PDFNameTree(ScannedDocument, PDFNameTree.Type.e_Pages);
-
-                    //The "savedTemplate{pageCount}" is used to give the template a dynamic name the more the user adds pages to the document.
-                    NewTemplateNameTree.Add($"savedTemplate{pageCount}", Page.GetDict());
-                    //Inserting a new page is needed as it'll be the blank page that the template will be added to
-                    Page = ScannedDocument.InsertPage(2, width, height);
-                    ScannedDocument.HidePageTemplate($"savedTemplate{pageCount}");
-
-                    Page = ScannedDocument.AddPageFromTemplate($"savedTemplate{pageCount}");
-                    ScannedDocument.SaveAs($"{FilePath.Split('.').First()}TemplatePage.pdf", (int)PDFDoc.SaveFlags.e_SaveFlagNoOriginal);
-                }
+                //"AddPageFromTemplate" is used to add a hidden page template to the document.
+                //Note that the "AddPageFromTemplate" method is used once per template name, upon calling the method, the hidden template is used and the name is released.
+                Page = ScannedDocument.AddPageFromTemplate(savedTemplate);
+                //I hardcoded "TemplatePage" to the file path to save the new template document to a new file rather than updating the original file
+                //As a personal convention, this can help in reconciling the original document with the template document
+                ScannedDocument.SaveAs($"{FilePath.Split('.').First()}TemplatePage.pdf", (int)PDFDoc.SaveFlags.e_SaveFlagNoOriginal);
+                int pageNumbers = ScannedDocument.GetPageCount();
             }
             catch (Exception ex)
             {
                 throw;
             }
         }
-
     }
 }
